f41a4a69bbfbcac3cda6ff9d4fb819df9076b6cf
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-05 23:13:08 +0530

    add refresh token model

--- src/api/v1/models/index.ts
+++ src/api/v1/models/index.ts
@@ -1,3 +1,4 @@
 export { default as User } from "./user";
-export { default as PasswordLogin } from "./password.login";
 export { default as OAuthLogin } from "./oauth.login";
+export { default as RefreshToken } from "./refreshToken";
+export { default as PasswordLogin } from "./password.login";
