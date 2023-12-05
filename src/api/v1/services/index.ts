573129ecc76fb9e042e4b9a1a71beab6f7aeb561
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-05 22:37:11 +0530

    add auth service in serive barrel export

--- src/api/v1/services/index.ts
+++ src/api/v1/services/index.ts
@@ -1,2 +1,3 @@
+export { default as authService } from "./auth.service";
 export { default as githubOAuthService } from "./github.oauth.service";
 export { default as googleOAuthService } from "./google.oauth.service";
