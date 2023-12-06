8386ab48b82ea5a86d7747c22c26c753168ce15d
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-06 21:04:05 +0530

    add user signup method

--- src/api/v1/controllers/auth.controller.ts
+++ src/api/v1/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ import {
     githubOAuthService,
     googleOAuthService,
 } from "../services";
-import cookieOptions from "../../../../config/cookie.config";
+import { cookieConfig } from "../../../../config";
 
 const githubOAuth = expressAsyncHandler(async (req, res, next) => {
     const code = req.query.code as string;
@@ -19,7 +19,7 @@ const githubOAuth = expressAsyncHandler(async (req, res, next) => {
     const { accessToken, refreshToken } = jwtService.generateTokens(authUser);
     await jwtService.manageRefreshToken(authUser, refreshToken);
 
-    res.cookie("refreshToken", refreshToken, cookieOptions).json({
+    res.cookie("refreshToken", refreshToken, cookieConfig).json({
         accessToken,
     });
 });
@@ -35,12 +35,26 @@ const googleOAuth = expressAsyncHandler(async (req, res, next) => {
     const { accessToken, refreshToken } = jwtService.generateTokens(authUser);
     await jwtService.manageRefreshToken(authUser, refreshToken);
 
-    res.cookie("refreshToken", refreshToken, cookieOptions).json({
+    res.cookie("refreshToken", refreshToken, cookieConfig).json({
         accessToken,
     });
 });
 
+const signUp = expressAsyncHandler(async (req, res, next) => {
+    const { first_name, family_name, username, password } = req.body;
+
+    const signedUpUser = await authService.handleUserSignUp({
+        first_name,
+        family_name,
+        username,
+        password,
+    });
+
+    res.status(201).json(signedUpUser);
+});
+
 export default {
+    signUp,
     githubOAuth,
     googleOAuth,
 };
