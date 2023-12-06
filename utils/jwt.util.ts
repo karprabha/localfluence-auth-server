26716667bdec153fd1374b7df73aba31f4474993
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-07 00:14:01 +0530

    add jwtdecode interface

--- utils/jwt.util.ts
+++ utils/jwt.util.ts
@@ -7,6 +7,12 @@ interface AuthUser {
     user_id: string;
 }
 
+interface JwtDecoded {
+    user_id: string;
+    iat: number;
+    exp: number;
+}
+
 const generateAccessToken = (payload: AuthUser) =>
     jwt.sign(payload, secretKey, {
         expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
@@ -17,20 +23,20 @@ const generateRefreshToken = (payload: AuthUser) =>
         expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     });
 
-const verifyAccessToken = (token: string) => {
+const verifyAccessToken = (token: string): JwtDecoded | null => {
     try {
         const tokenWithoutBearer = token.split(" ")[1];
         const decoded = jwt.verify(tokenWithoutBearer, secretKey);
-        return decoded;
+        return decoded as JwtDecoded;
     } catch (error) {
         return null;
     }
 };
 
-const verifyRefreshToken = (token: string) => {
+const verifyRefreshToken = (token: string): JwtDecoded | null => {
     try {
         const decoded = jwt.verify(token, refreshTokenKey);
-        return decoded;
+        return decoded as JwtDecoded;
     } catch (error) {
         return null;
     }
